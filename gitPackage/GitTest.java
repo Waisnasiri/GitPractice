ff26cbf75adbd8c8a8144ae5188fe03108aecc8f
AhmadWais <ahmad.nasiri@tekschool.us>
2022-11-08 22:46:46 -0800

    this is for remote

--- gitPackage/GitTest.java
+++ gitPackage/GitTest.java
@@ -1,13 +1,10 @@
 package gitPackage;
 
-import java.util.Scanner;
 
 public class GitTest {
 
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println(scanner + "do you hangry?");
 		
 	
 		String hungry = "Yes";
